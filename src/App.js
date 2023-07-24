ce98dbe8310af6bb5076a26887ec4124fd64bb94
connpass1 <connpass@gmail.com>
2023-07-24 21:21:14 +0300

    change css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+
+import { Layout, theme } from 'antd';
 import React, { useEffect } from "react";
 import RouteTable from "./components/RouteTable";
 import Map from "./components/Map";
 import ErrorMessage from "./components/ErrorMessage";
 import { FETCH_POLYlINE_START } from "./reducers"
 import { useDispatch } from 'react-redux'
+
+const { Content, Sider } = Layout;
+
 const App = () => {
-    console.log(process.env.NODE_ENV);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     const dispatch = useDispatch();
     const [route_number, setRoute_number] = React.useState(null)
     useEffect(() => {
         if (route_number !== null) dispatch({ type: FETCH_POLYlINE_START, route_number: route_number })
     }, [dispatch, route_number]);
-    return <main>
-        <div>  <RouteTable route_number={route_number} setRoute_number={setRoute_number} />
-            <ErrorMessage />
-        </div>
-        <Map route_number={route_number} />
-    </main>
-}
-export default App
+    return (
+        <Layout>
+            <Content style={{ padding: '0 10px' }}>
+                <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
+                    <Sider style={{ background: colorBgContainer }} width={500}>
+                        <RouteTable route_number={route_number} setRoute_number={setRoute_number} />
+                        <ErrorMessage />
+                    </Sider>
+                    <Content style={{ padding: '0 12px', minHeight: 480 }}>  <Map route_number={route_number} /> </Content>
+                </Layout>
+            </Content>
+
+        </Layout>
+    );
+};
+
+export default App;
